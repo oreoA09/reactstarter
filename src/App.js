c4e1a70a8b225c7e6475ddbe6ba272359294e59f
oreoA09 <oreo.akanni@gmail.com>
2022-10-08 20:42:55 +0000

    useStateArray execrise

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import ErrorExample from './tutorial/1-useState/setup/1-error-example'
 import UseStates from './tutorial/1-useState/setup/2-useState-basics'
+import UseStateArray from './tutorial/1-useState/setup/3-useState-array'
 
 function App() {
   return (
     <div className='container'>
       <ErrorExample />
       <UseStates />
+      <UseStateArray />
     </div>
   )
 }
