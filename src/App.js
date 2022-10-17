0acd6694d869e6b971dbb04a9e94c997c4b6b989
oreoA09 <oreo.akanni@gmail.com>
2022-10-17 19:34:38 +0000

    useState counter

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react'
 import ErrorExample from './tutorial/1-useState/setup/1-error-example'
 import UseStates from './tutorial/1-useState/setup/2-useState-basics'
 import UseStateArray from './tutorial/1-useState/setup/3-useState-array'
+import UseStateObject from './tutorial/1-useState/setup/4-useState-object'
+import UseStateCounter from './tutorial/1-useState/setup/5-useState-counter'
 
 function App() {
   return (
     <div className='container'>
-      <ErrorExample />
-      <UseStates />
-      <UseStateArray />
+      {/* <ErrorExample /> */}
+      {/* <UseStates /> */}
+      {/* <UseStateArray /> */}
+      <UseStateObject />
+      <UseStateCounter />
     </div>
   )
 }
